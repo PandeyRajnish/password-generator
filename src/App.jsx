128d0172ef38939b9b7d6088567b33baccaa450a
Rajnish Pandey <piyushwinner1997@gmail.com>
2024-02-08 10:52:51 +0530

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <p>Password Generator</p>
-      </div>
+      <h1 className="text-4xl text-center text-white">Password Generator</h1>
     </>
   );
 }
